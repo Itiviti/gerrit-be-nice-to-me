82af83ab3f71c392ad329d92c367552d79fbc263
Francois Valdy <francois.valdy@gmail.com>
2014-03-14 20:44:15 +0100

    Updated to UL
    - Added blue color for self comments
    - Replaced open/close hack by Gerrit RPC
    - Added parent id to patches headers to easily spot rebases

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 ( function( $ ) {
 
+//
+var xGerritAuth;
+var gerrit_rpc_base = "/gerrit_ui/rpc/";
+var current_change_id;
+var gerrit_request_id = 1;
+
 function colorComment( $commentPanel ) {
+	var author = $commentPanel.find( '.commentPanelAuthorCell' ).text();
+	var username = $('.menuBarUserName').text()
 	var heading = $commentPanel.find( '.commentPanelMessage p' ).eq( 0 ).text(),
 		color = '#aaa';
-	if ( heading.match( /Code\-Review\-2$/ ) || heading.match( /Verified\-2$/ ) ) {
+	if (author === username) {
+		color = 'blue';
+	} else if ( heading.match( /Code\-Review\-2$/ ) || heading.match( /Verified\-2$/ ) ) {
 		color = '#C90505';
 	} else if ( heading.match( /Code\-Review\-1$/ ) || heading.match( /Verified\-1$/ ) ) {
 		color = 'red';
@@ -19,67 +29,88 @@ function colorComment( $commentPanel ) {
 	} );
 }
 
+function postJSON(_url, data, func) {
+	$.ajax({type: "POST",
+			url: _url,
+			dataType: "json",
+			data: data,
+			headers: {"Content-Type":"application/json; charset=UTF-8",
+					  "Accept":"application/json; application/jsonrequest; text/html",
+					  "X-Gerrit-Auth": xGerritAuth
+					 }
+			})
+	.done(func)
+	.fail(function(a){func("badvalue")});
+}
+
+function gerritJsonRPC(service, func, params, cb) {
+	gerrit_request_id +=1;
+	var url = document.location.origin+gerrit_rpc_base+service;
+	var jsonData = '{"jsonrpc":"2.0","method":"'+func+'","params":'+JSON.stringify(params)+',"id":'+gerrit_request_id+',"xsrfKey":"'+ xGerritAuth+'"}';
+	postJSON(url, jsonData, cb);
+}
+
+function getDetail(id, psid, cb) {
+	gerritJsonRPC("ChangeDetailService", "patchSetDetail2",[null,{"changeId":{"id":id},"patchSetId":psid},null], function(r){
+		cb(r.result);
+	});
+}
+
+function parseChangeId(href) {
+	var orig = href.substring(8)
+	orig = orig.substring(orig.indexOf('/'))
+	if (orig.indexOf('/c/') != -1)
+		orig = orig.substring(orig.indexOf('/c/')+3)
+	else if (orig.indexOf('/changes/') != -1)
+		orig = orig.substring(orig.indexOf('/changes/')+9)
+	else 
+		orig = orig.substring(1)
+	if (orig.indexOf('/') != -1)
+		orig = orig.substring(0, orig.indexOf('/'))
+	return orig;
+}
+
 function listener( ev ) {
 	var $t = $( ev.target ), $owner, author, action;
 	if ( $t.hasClass( 'commentPanel' ) ) { // force open comment panel
 		author = $t.find( '.commentPanelAuthorCell' ).text();
 		action = $t.find( '.commentPanelSummary' ).text();
-		if ( author === 'jenkins-bot' ||
+		if ( author === 'builder builder' || author === 'Review Bot' ||
 			action.indexOf( 'Uploaded patch set' ) === 0  ||
 			action.match( /was rebased$/ ) ) {
 			 // make jenkins comments less prominent
 			$t.find( '.commentPanelHeader' ).css( 'opacity', 0.6 );
-		} else {
+		} else if (action.match( /…$/)) {
+			$t.find( '.commentPanelSummary' ).hide();
 			$t.find( '.commentPanelContent' ).show();
 		}
 		colorComment( $t );
-	} else if ( $t.hasClass( 'gwt-DisclosurePanel' ) ) { // open patchset
-		$( '.gwt-DisclosurePanel-closed tbody tr' ).trigger( 'click' ); // HACK! not optimal
-	} else if ( $t.children( '.changeTable' ).length > 0 ) {
-
-		var comments = 0;
-		$t.find( '.changeTable .commentCell' ).each( function() { // count comments
-			var text = $( this ).text(), newCount;
-			if ( text ) { // not empty
-				text = text.replace( ' comment' ); // errgg hacky sorry
-				newCount = parseInt( text, 10 );
-				if ( !isNaN( newCount ) ) { // check we got a number
-					comments += newCount; // add it to the count
-				}
-			}
-		} );
-		$owner = $t.parents( '.gwt-DisclosurePanel' );
-		$( '<a class="downloadLink">' ).text( comments + ' comments' ).
-			appendTo( $owner.find( 'tr' ).eq( 0 ).find( 'td' ).eq( 2 ) );
-		$owner.find( 'tbody tr' ).trigger( 'click' );
-	} else if ( $t.hasClass( 'gwt-Image' ) ) {
-		// hacky way of detecting page load on my reviews page
-		if ( $t.attr( 'src' ) === 'https://gerrit.wikimedia.org/r/gerrit_ui/clear.cache.gif' ) {
-
-			// change the style of all rows to reflect the patchset current score
-			$( '.changeTable tr' ).each( function() {
-				var color, className;
-				if ( $( this ).find( '.negscore.singleLine' ).length > 0 ) {
-					className = 'negscore';
-					color = 'red';
-				} else if ( $( this ).find( '.posscore.singleLine' ).length === 2 ) {
-					className = 'posscore';
-					color = '#08a400';
-				} else {
-					className = false;
-				}
-				if ( className ) {
-					$( this ).find( 'td' ).each( function() {
-						if ( !$( this ).hasClass( 'iconCell' ) ) {
-							$( this ).addClass( className + ' dataCell' ).
-								find( 'a' ).attr( 'style', 'color: ' + color + ' !important;' ); // ergg WHY GERRIT WHY?!!
-						}
-					} );
-				}
-			} );
-		}
+	} else if ( $t.hasClass( 'gwt-InlineHyperlink' ) && $t.text() === 'Permalink') {
+		current_change_id = parseChangeId($t.attr('href'));
+	} else if ( $t.hasClass( 'gwt-DisclosurePanel' ) ) {
+		var psText = $t.find( 'tr' ).eq( 0 ).find( 'td' ).eq( 2 ).text().replace('Patch Set ','')
+	    getDetail(current_change_id, psText, function(r){
+			var parent = r.info.parents[0].id.id;
+			var comments = 0;
+			r.patches.forEach(function(a) {
+				comments += a.nbrComments;
+			});
+			if (comments > 0)
+				$( '<span/>' ).text( ' (' + comments + ')').appendTo( $t.find( 'tr' ).eq( 0 ).find( 'td' ).eq( 2 ) );
+			$( '<span/>' ).text( ' / ' + parent ).appendTo( $t.find( '.patchSetRevision' ) );
+		})
 	}
 }
+
 document.addEventListener( 'DOMNodeInserted', listener, false );
+
+$.get(document.location.origin.toString())
+.done(function(res) {
+	xGerritAuth = res.split("xGerritAuth")
+	if (xGerritAuth.length>1)
+		xGerritAuth = xGerritAuth[1].split('=')[1].split('"')[1]
+	else
+		xGerritAuth=""
+});
 
 } )( jQuery );
